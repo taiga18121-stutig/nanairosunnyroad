fe332dc43ba43e43b77f9c70022595f9fd24da25
taiga18121-stutig <taiga18121@gmail.com>
2026-02-06 05:04:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// エラーを強制的に回避する設定
+window.jQuery.isElectron = function() { return false; };
+
 $(function(){
-    // ブラウザでの動作を安定させるための呪文
-    $.isElectron = function(){ return false; };
-    
-    // ゲームの初期化を実行
-    tyrano.plugin.kag.init();
+    // 少しだけ間を置いてから起動（読み込み待ち）
+    setTimeout(function(){
+        tyrano.plugin.kag.init();
+    }, 100);
 });
